23c3d35b3a71ce1f15eda593b901f916a1ca4198
Juliana Cardozo <cardozojuliana908@gmail.com>
2020-11-02 13:28:38 -0500

    Dibuja Marcos

--- PuzzleBlock.js
+++ PuzzleBlock.js
@@ -1,9 +1,10 @@
 ﻿class MarcosCuadros {
-    constructor(x, y, ancho, alto) {
+    constructor(x, y, ancho, alto, figura) {
         this.alto = alto;
         this.ancho = ancho;
         this.x = x;
         this.y = y;
+        this.figura = figura;
     }
     dibujar() {
         var c = document.getElementById("myCanvas");
@@ -11,34 +12,44 @@
         ctx.beginPath();
         ctx.lineWidth = "2";
         ctx.strokeStyle = "black";
-        ctx.rect(this.x, this.y, this.ancho, this.alto);
+        if (this.figura == 1) {
+            ctx.rect(this.x, this.y, this.ancho, this.alto);
+        }
+        else {
+            ctx.ellipse(this.x, this.y, this.ancho, this.alto, 0, 0, (2 * Math.PI));
+        }
         ctx.stroke();
         ctx.fillStyle = "#F0C89F";
         ctx.fill();
         ctx.beginPath();
-        ctx.rect(this.x + 15, this.y + 15, this.ancho - 30, this.alto - 30);
+        if (this.figura == 1) {
+            ctx.rect(this.x + 15, this.y + 15, this.ancho - 30, this.alto - 30);
+        }
+        else {
+            ctx.ellipse(this.x + 15, this.y + 15, this.ancho - 30, this.alto - 30, 0, 0, (2 * Math.PI));
+        }
         ctx.stroke();
         ctx.fillStyle = "#B9834D";
         ctx.fill();
-        ctx.clearRect(this.x + 30, this.y + 30, this.ancho - 60, this.alto - 60);
-        ctx.beginPath();
-        ctx.lineWidth = "1";
-        ctx.moveTo(this.x + 30, this.y + 30);
-        ctx.lineTo(this.x + 15, this.y + 15);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(this.x + 30, this.y + (this.alto- 30));
-        ctx.lineTo(this.x + 15, this.y + (this.alto -15));
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(this.x + (this.ancho - 30), this.y +30);
-        ctx.lineTo(this.x + (this.ancho - 15), this.y + 15);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(this.x + (this.ancho - 30), this.y + (this.alto - 30));
-        ctx.lineTo(this.x + (this.ancho - 15), this.y + (this.alto - 15));
-        ctx.stroke();
-    }
-         
-    
+        if (this.figura == 1) {
+            ctx.clearRect(this.x + 30, this.y + 30, this.ancho - 60, this.alto - 60);
+            ctx.beginPath();
+            ctx.lineWidth = "1";
+            ctx.moveTo(this.x + 30, this.y + 30);
+            ctx.lineTo(this.x + 15, this.y + 15);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(this.x + 30, this.y + (this.alto - 30));
+            ctx.lineTo(this.x + 15, this.y + (this.alto - 15));
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(this.x + (this.ancho - 30), this.y + 30);
+            ctx.lineTo(this.x + (this.ancho - 15), this.y + 15);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(this.x + (this.ancho - 30), this.y + (this.alto - 30));
+            ctx.lineTo(this.x + (this.ancho - 15), this.y + (this.alto - 15));
+            ctx.stroke();
+        }
+    }   
 }
