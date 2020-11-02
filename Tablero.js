803b51e5c57a0ab735e001f03e7a9677f95f0e5b
Juliana Cardozo <cardozojuliana908@gmail.com>
2020-11-02 15:52:14 -0500

    Funciones Mejoradas

--- Tablero.js
+++ Tablero.js
@@ -1,28 +1,53 @@
 // JavaScript source code
-var matrix = new Array[10][10];
+var matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+var score;
+var FilasCompletas;
 function EliminarColumnaFila() {
     for (var iME = 9; iME > 0; iME--) {
-        var siEliminarFila = new Boolean(true);
+        var siEliminarFila =  Boolean(true);
         for (var jME = 0; jME < 10; jME++) {
-            if (matrix[iME][jME] == 0) {
+            if (matrix[iME][jME] === 0) {
                 siEliminarFila = false;
             }
         }
         if (siEliminarFila) {
-            for (var k = iME; k >= 0; k--) {
-                for (var m = 0; m < 16; m++) {
-                    if (k != 0) {
-                        matrix[k][m] = matrix[k - 1][m];
-                    } else {
-                        matrix[k][0] = 1;
-                        matrix[k][m] = 0;
-                        matrix[k][15] = 1;
-                    }
-                }
+            var k = iME;
+            for (var m = 0; m < 10; m++) {
+                matrix[k][m] = 0;
             }
-            iME = iME + 1;
             score += 50;
             FilasCompletas += 1;
+        }
+    }
+    for (var iME = 9; iME > 0; iME--) {
+        var siEliminarColumna = Boolean(true);
+        for (var jME = 0; jME < 10; jME++) {
+            if (matrix[jME][iME] === 0) {
+                siEliminarColumna = false;
+            }
+        }
+        if (siEliminarColumna) {
+            var k = iME;
+            for (var m = 0; m < 10; m++) {
+                matrix[m][k] = 0;
+            }
+            score += 50;
+            FilasCompletas += 1;
+        }
+    }
+    for (var i in matrix) {
+        console.log("row " + i);
+        for (var j in matrix[i]) {
+            console.log(" " + matrix[i][j]);
         }
     }
 }
