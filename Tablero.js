b2185fe0d7d22f1a3e26630f909d56f800f74c65
Juliana Cardozo <cardozojuliana908@gmail.com>
2020-11-05 20:27:45 -0500

    Clase Tablero

--- Tablero.js
+++ Tablero.js
@@ -1,53 +1,73 @@
 // JavaScript source code
-var matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 var score;
 var FilasCompletas;
-function EliminarColumnaFila() {
-    for (var iME = 9; iME > 0; iME--) {
-        var siEliminarFila =  Boolean(true);
-        for (var jME = 0; jME < 10; jME++) {
-            if (matrix[iME][jME] === 0) {
-                siEliminarFila = false;
-            }
-        }
-        if (siEliminarFila) {
-            var k = iME;
-            for (var m = 0; m < 10; m++) {
-                matrix[k][m] = 0;
-            }
-            score += 50;
-            FilasCompletas += 1;
+var TableroMemoria = Array(this.columna);
+class Tablero {
+    constructor(columna, fila) {
+        this.columna = columna;
+        this.fila = fila;
+    }
+    crearTablero() {
+        for (var x = 0; x < this.fila; x++) {
+            TableroMemoria[x] = Array(this.fila);
+            for (var j = 0; j < this.columna; j++) {
+                TableroMemoria[x][j] = "#292B4A";}
         }
     }
-    for (var iME = 9; iME > 0; iME--) {
-        var siEliminarColumna = Boolean(true);
-        for (var jME = 0; jME < 10; jME++) {
-            if (matrix[jME][iME] === 0) {
-                siEliminarColumna = false;
+    dibujarTablero() {
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.lineWidth = "1";
+        ctx.strokeStyle = "#52568E";
+        for (var co = 0; co < this.columna; co++) {
+            for (var f = 0; f < this.fila; f++) {
+                ctx.beginPath();
+                ctx.rect(310 + (co * 48), 45 + (f * 48), 48, 48);
+                ctx.stroke();
+                ctx.fillStyle = TableroMemoria[co][f];
+                ctx.fill();
             }
         }
-        if (siEliminarColumna) {
-            var k = iME;
-            for (var m = 0; m < 10; m++) {
-                matrix[m][k] = 0;
+          }
+    EliminarColumnaFila() {
+        for (var iME = 11; iME >= 0; iME--) {
+            var siEliminarFila = Boolean(true);
+            for (var jME = 0; jME < 12; jME++) {
+                if (TableroMemoria[iME][jME] == "#292B4A") {
+                    siEliminarFila = false;
+                }
             }
-            score += 50;
-            FilasCompletas += 1;
+            if (siEliminarFila) {
+                var k = iME;
+                for (var m = 0; m < 12; m++) {
+                    TableroMemoria[k][m] = "#292B4A";
+                }
+                score += 50;
+                FilasCompletas += 1;
+            }
+        }
+        for (var iME = 11; iME > 0; iME--) {
+            var siEliminarColumna = Boolean(true);
+            for (var jME = 0; jME < 12; jME++) {
+                if (TableroMemoria[jME][iME] == "#292B4A") {
+                    siEliminarColumna = false;
+                }
+            }
+            if (siEliminarColumna) {
+                var k = iME;
+                for (var m = 0; m < 12; m++) {
+                    TableroMemoria[m][k] = "#292B4A";
+                }
+                score += 50;
+                FilasCompletas += 1;
+            }
+        }
+        for (var i in TableroMemoria) {
+            console.log("row " + i);
+            for (var j in TableroMemoria[i]) {
+                console.log(" " + TableroMemoria[i][j]);
+            }
         }
     }
-    for (var i in matrix) {
-        console.log("row " + i);
-        for (var j in matrix[i]) {
-            console.log(" " + matrix[i][j]);
-        }
-    }
+
 }
