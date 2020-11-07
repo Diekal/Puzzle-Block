0a6369f0cc21cb6e04ca09508c62c72ea3028175
Juliana Cardozo <cardozojuliana908@gmail.com>
2020-11-07 18:15:32 -0500

    Agregar Bombas

--- Puzzleblock.js
+++ Puzzleblock.js
@@ -4,24 +4,23 @@ var SegundoMarco;
 var TercerMarco;
 var CuartoMarco;
 var poliomino;
+var poliomino2;
 var tablero;
+var Segundos;
 //var polyomino2;
 //var polyomino3;
 
 function setup() {
-<<<<<<< HEAD
     createCanvas(1200, 660);
     //polyomino2 = createPolyomino(elegir_p(),100,100);
     //polyomino3 = createPolyomino(elegir_p(),200,200);
-=======
-    createCanvas(1200, 700);
->>>>>>> c31fe5dbb2b66cda1b335b415365cb7a4272b56f
     PrimerMarco = new MarcosCuadros(25, 50, 220, 550, 1);
     SegundoMarco = new MarcosCuadros(280, 15, 635, 635, 1);
     TercerMarco = new MarcosCuadros(960, 330, 200, 300, 1);
     CuartoMarco = new MarcosCuadros(1050, 165, 200, 250, 2);
     tablero = new Tablero(12, 12);
-    poliomino = new Poliomino(65,65,45);
+    poliomino = new Poliomino(65,65,48);
+    poliomino2 = new Poliomino(65,65,48);
 }
 
 function draw() {
@@ -33,20 +32,20 @@ function draw() {
     TercerMarco.dibujar();
     CuartoMarco.dibujar();
     tablero.dibujarTablero();
-<<<<<<< HEAD
-=======
-    poliomino.dibujar_p();
->>>>>>> c31fe5dbb2b66cda1b335b415365cb7a4272b56f
     tablero.EliminarColumnaFila();
     tablero.dibujarTablero();
     poliomino.dibujar_p();
+    if ((millis() - Segundos) >= 20){
+        tablero.Bomba();
+        Segundos = millis();
+        console.log("Funciona")
+    }
 }
 
 function mouseDragged() {
     poliomino.move_p(mouseX,mouseY);
   }
 function mouseReleased(){
-         
 }
 // no entendi para que es esta funcion pero pues estaba en la plantilla
 function debugPolyomino(poliomino) {
